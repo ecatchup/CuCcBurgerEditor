e59e1043c6f7cd0cc735564324283b207bc1510f
kawase <kawase@catchup.co.jp>
2025-06-17 11:04:26 +0900

    他のメソッドと同じ命名規則に変更する

--- tests/TestCase/View/Helper/CuCcBurgerEditorHelperTest.php
+++ tests/TestCase/View/Helper/CuCcBurgerEditorHelperTest.php
@@ -47,15 +47,29 @@ class CuCcBurgerEditorHelperTest extends BcTestCase
         BcUtil::includePluginClass('CuCcBurgerEditor');
     }
 
-    public function testControlReturnsEmptyStringIfPreviewOptionIsSet()
+    /**
+     * control()のテスト
+     *
+     * - previewオプションがtrueなら空文字を返す
+     * - previewオプションがなければBcAdminForm->editorが呼ばれ、その戻り値を返す
+     */
+    public function test_control()
     {
-        $field = (object)['name' => 'test', 'custom_field' => (object)['meta' => ['CuCcBurgerEditor' => ['editor_use_draft' => true]]]];
+        // previewオプションがtrueの場合
+        $field = (object)[
+            'name' => 'test',
+            'custom_field' => (object)[
+                'meta' => [
+                    'CuCcBurgerEditor' => [
+                        'editor_use_draft' => true
+                    ]
+                ]
+            ]
+        ];
         $result = $this->CuCcBurgerEditorHelper->control($field, ['preview' => true]);
-        $this->assertSame('', $result);
-    }
+        $this->assertSame('', $result, 'previewオプションがtrueなら空文字を返す');
 
-    public function testControlCallsEditorWithCorrectArguments()
-    {
+        // previewオプションがない場合
         $field = (object)[
             'name' => 'test_field',
             'custom_field' => (object)[
@@ -76,6 +90,19 @@ class CuCcBurgerEditorHelperTest extends BcTestCase
             )
             ->willReturn($expected);
         $result = $this->CuCcBurgerEditorHelper->control($field, $options);
-        $this->assertSame($expected, $result);
+        $this->assertSame($expected, $result, 'previewオプションがなければeditorの戻り値を返す');
+    }
+
+    /**
+     * get()のテスト
+     *
+     * - fieldValueをそのまま返す
+     */
+    public function test_get()
+    {
+        $fieldValue = '<p>テスト本文</p>';
+        $link = $this->createMock(\BcCustomContent\Model\Entity\CustomLink::class);
+        $result = $this->CuCcBurgerEditorHelper->get($fieldValue, $link);
+        $this->assertSame($fieldValue, $result, 'fieldValueをそのまま返す');
     }
 }
